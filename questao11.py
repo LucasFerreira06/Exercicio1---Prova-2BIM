36f093a0237268545100c754fb9ac2abb496f0d0
Lucas Ferreira <lucasnatacao@gmail.com>
2016-08-30 15:16:15 -0300

    Adicionando questao 11

---
+++ questao11.py
@@ -0,0 +1,8 @@
+Numero1 = int(input("Digite um número:"))
+Numero2 = int(input("Digite um número:"))
+Soma = 0
+
+for Numero1 in range(Numero1 + 1,Numero2):
+    print(Numero1)
+    Soma = Soma + Numero1
+print("A soma dos números é:",Soma) 
