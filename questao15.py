1ac4054553eafb8390664351eaf550a748abf0dd
Lucas Ferreira <lucasnatacao@gmail.com>
2016-08-30 17:28:03 -0300

    Adicionando questao 15

---
+++ questao15.py
@@ -0,0 +1,17 @@
+Numero = int(input("Digite o n-ésimo número:"))
+W = 1
+X = 1
+Y = 0
+Z = 1
+print(X)
+print(X)
+
+while(Z <= Numero - 2):
+    Y = W + X
+    print(Y)
+    Z = Z + 1 
+    W = X
+    X = Y
+print("Fim da série") 
+    
+    
