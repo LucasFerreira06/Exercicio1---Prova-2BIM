3729396a7697763bd2efbfa96b6d88a467def217
Lucas Ferreira <lucasnatacao@gmail.com>
2016-09-02 20:22:19 -0300

    Adicionando questao 25

---
+++ questao25.py
@@ -0,0 +1,18 @@
+Numero = int(input("Número de pessoas:"))
+Auxiliar = 0
+
+for i in range(1,Numero + 1):
+    Idade = int(input("Idade das pessoas:"))
+    Soma = Auxiliar + Idade
+    Auxiliar = Soma
+
+Media = Soma / Numero
+
+if (Media >= 0 and Media <= 25):
+    print("Turma Jovem!")
+elif (Media >= 26 and Media <= 60):
+    print("Turma Adulta!")
+elif (Media > 60):
+    print("Turma Idosa!")
+
+
