6c6d3f9c4283fa53781823c93dd5b0d18e16faad
Lucas Ferreira <lucasnatacao@gmail.com>
2016-09-03 16:10:56 -0300

    Adicionando questao 19

---
+++ questao19.py
@@ -0,0 +1,24 @@
+QtdNumeros = int(input("Digite a quantidade de números que você quer comparar:"))
+Numero = int(input("Digite um número:"))
+Maior = Numero
+Menor = Numero
+Soma = 0
+Auxiliar = 1
+
+if((Numero > 0 and Numero <= 1000)or Numero == 1):
+    while (Auxiliar <= QtdNumeros):
+        if (Auxiliar < QtdNumeros):
+            Numero = int(input("Digite um número:"))
+        if (Numero > Maior):
+            Maior = Numero
+        elif (Numero < Menor):
+            Menor = Numero
+        Soma = Maior + Menor 
+        Auxiliar = Auxiliar + 1
+
+    print("Maior número é:",Maior)
+    print("Menor número é:",Menor)
+    print("Soma igual a:",Soma)
+
+else:
+    print("Valor Inválido") 
