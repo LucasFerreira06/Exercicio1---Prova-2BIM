b83b49d1798558913e590d8398eedaa8219768c4
Lucas Ferreira <lucasnatacao@gmail.com>
2016-09-03 14:41:31 -0300

    Adicionando questao 38

---
+++ questao38.py
@@ -0,0 +1,25 @@
+Ano = 1996
+Percen = 0.015
+Salario = 1015
+
+while(Ano <= 2016):
+    Ano = Ano + 1
+    Aumento = Salario * Percen
+    Salario = Salario + Aumento
+    Percen = Percen * 2
+    
+print("Salário Atual:",Salario)
+
+print("Alterando o programa - (O salário será fornecido pelo usuário)")
+
+Ano = 1996
+Percen = 0.015
+Salario = eval(input("Digite o salário inicial:")
+
+while(Ano <= 2016):
+    Ano = Ano + 1
+    Aumento = Salario * Percen
+    Salario = Salario + Aumento
+    Percen = Percen * 2
+               
+print("Salário Atual:",Salario)
