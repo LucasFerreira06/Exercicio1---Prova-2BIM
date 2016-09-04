6c9a97aa2fbf333b165df84b00a2a9a1836d2166
Lucas Ferreira <lucasnatacao@gmail.com>
2016-09-03 22:52:05 -0300

    Adicionando questao 30

--- questao30.py
+++ questao30.py
@@ -1,7 +1,9 @@
-Preço = 0.18
+Preço = eval(input("Digite o preço do pão R$:"))
+print("Preço do pão R$:",Preço)
+print("Panificadora Pão de Ontem - Tabela de preços")
 Auxiliar = 1
 
 for Tabela in range(0,50):
-    print(Auxiliar,"- R$ %.2f" %Preço)
+    print(Auxiliar,"- R$ %.2f" %(Preço * Auxiliar))
     Auxiliar = Auxiliar + 1
-    Preço = Preço + 0.18
+    
